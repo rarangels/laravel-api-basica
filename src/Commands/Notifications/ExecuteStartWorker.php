2704ea6fff3fbcc94e14cde3497ccb77b84b177d
Rafael Agustin Rangel Sandoval <rarangels@unal.edu.co>
2020-08-12 18:39:55 -0500

    Se añade el worker de las jobs

---
+++ src/Commands/Notifications/ExecuteStartWorker.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rarangels
+ * Date: 8/07/20
+ * Time: 10:07 a. m.
+ * Author: Rafael Agustin Rangel Sandoval <rarangels93@gmail.com>
+ */
+
+namespace rarangels\Commands\Notifications;
+
+/**
+ * Class ExecuteStartWorker
+ *
+ * @package App\Helpers\Classes
+ * @author Rafael Agustin Rangel Sandoval <rarangels93@gmail.com>
+ */
+class ExecuteStartWorker
+{
+    /**
+     * @var
+     */
+    public $pid;
+
+    /**
+     * @var string
+     */
+    private $command;
+
+    /**
+     * @var string
+     */
+    private $command_complement;
+
+    /**
+     * ExecuteStartWorker constructor.
+     */
+    public function __construct()
+    {
+        $this->command = "php ".base_path()."/artisan queue:work";
+        $this->command_complement = " >> ".storage_path()."/logs/jobs.log 2>&1 &";
+        //$this->run();
+    }
+
+    /**
+     * @param $command
+     * @return string
+     * @author Rafael Agustin Rangel Sandoval <rarangels93@gmail.com>
+     */
+    private function new_process($command): string
+    {
+        $this->executeCommand($command);
+
+        return $this->getPIDofCommand($this->command);
+    }
+
+    /**
+     * @author Rafael Agustin Rangel Sandoval <rarangels93@gmail.com>
+     */
+    public function run(): void
+    {
+        $output = $this->getPIDofCommand($this->command);
+        if (is_null($output)) {
+            $output = $this->new_process($this->command.$this->command_complement);
+        }
+        $this->pid = (int) $output;
+    }
+
+    /**
+     * @param $command
+     * @return string|null
+     * @author Rafael Agustin Rangel Sandoval <rarangels93@gmail.com>
+     */
+    private function executeCommand($command)
+    {
+        return shell_exec($command);
+    }
+
+    /**
+     * @param $command
+     * @return int|null
+     * @author Rafael Agustin Rangel Sandoval <rarangels93@gmail.com>
+     */
+    private function getPIDofCommand($command)
+    {
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            return $this->getPIDoFCommandWindows($command);
+        } else {
+            return $this->getPIDoFCommandLinux($command);
+        }
+    }
+
+    /**
+     * @param $command
+     * @return int|null
+     * @author Rafael Agustin Rangel Sandoval <rarangels93@gmail.com>
+     */
+    private function getPIDoFCommandLinux($command)
+    {
+        $result = $this->executeCommand('pgrep -af "'.$command.'"');
+
+        $processes = explode(PHP_EOL, $result);
+        foreach ($processes as $index => $process) {
+            $pid = intval(preg_replace('/[^0-9]\s+/', '', $process));
+            $comando = str_replace($pid.' ', '', $processes[$index]);
+            if ($command === $comando) {
+                return $pid;
+            }
+        };
+
+        return null;
+    }
+
+    /**
+     * @param $command
+     * @return string
+     * @author Rafael Agustin Rangel Sandoval <rarangels93@gmail.com>
+     */
+    private function getPIDoFCommandWindows($command)
+    {
+        return '1';
+    }
+}
---
+++ src/Commands/Notifications/WorkerStart.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use rarangels\Commands\Notifications\ExecuteStartWorker;
+
+/**
+ * Class StartWorker
+ *
+ * @package App\Console\Commands
+ * @author Rafael Agustin Rangel Sandoval <rarangels93@gmail.com>
+ */
+class WorkerStart extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'rarangels-worker-notifications:start';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Inicializador del worker de las Jobs';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            $this->info($this->getPIDoFCommandWindows("php ".base_path()."/artisan queue:work"));
+            $process = new ExecuteStartWorker();
+            $process->pid = null;
+            //$process->run();
+        } else {
+            $process = new ExecuteStartWorker();
+            $process->run();
+        }
+        if (is_null($process->pid)){
+            $this->info(Carbon::now().': El worker de las Jobs se encuentra detenido.');
+        }
+        $this->info(Carbon::now().': El worker de las Jobs se encuentra en ejecución mediante el PID: '.$process->pid);
+    }
+
+    private function getPIDoFCommandWindows($command)
+    {
+        $this->info(Carbon::now().': soy windows');
+        $this->info($command);
+
+        return '1';
+    }
+}
