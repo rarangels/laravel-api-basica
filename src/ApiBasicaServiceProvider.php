cd7fdef08e465450000ff1725ab9d6c752445ee6
Rafael Agustin Rangel Sandoval <rarangels@unal.edu.co>
2020-08-12 19:09:01 -0500

    Se agrega el service providers para autodescubrir el comando

---
+++ src/ApiBasicaServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rarangels
+ * Date: 12/08/20
+ * Time: 6:59 p. m.
+ * Author: Rafael Agustin Rangel Sandoval <rarangels93@gmail.com>
+ */
+namespace Rarangels\ApiBasica;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class RarangelsApiBasicaServiceProvider
+ *
+ * @package Rarangels\ApiBasica
+ * @author Rafael Agustin Rangel Sandoval <rarangels93@gmail.com>
+ */
+class ApiBasicaServiceProvider extends ServiceProvider
+{
+    /**
+     * @author Rafael Agustin Rangel Sandoval <rarangels93@gmail.com>
+     */
+    public function boot(){
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                Commands\Notifications\ExecuteStartWorker::class,
+            ]);
+        }
+    }
+}
