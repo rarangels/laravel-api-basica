5dc6715b0c4a7c2614addca077606879c4b7c87e
Rafael Agustin Rangel Sandoval <rarangels@unal.edu.co>
2020-08-15 23:18:01 -0500

    Se reordenan las publicaciones de archivos

--- src/ApiBasicaServiceProvider.php
+++ src/ApiBasicaServiceProvider.php
@@ -22,29 +22,43 @@ use Illuminate\Support\ServiceProvider;
 class ApiBasicaServiceProvider extends ServiceProvider
 {
     /**
-     * @param \Illuminate\Filesystem\Filesystem $filesystem
      * @author Rafael Agustin Rangel Sandoval <rarangels93@gmail.com>
      */
-    public function boot(Filesystem $filesystem)
+    public function boot()
     {
         if (function_exists('config_path')) {
-            $this->publishes([
-                __DIR__.'/../config/api-basica.php' => config_path('api-basica.php'),
-            ], 'config');
-
-            $this->publishes([
-                __DIR__.'/../database/migrations/create_applications_table.php.stub' => $this->getMigrationFileName($filesystem),
-            ], 'migrations');
-
-            $this->publishes([
-                __DIR__.'/../database/migrations/create_configurations_table.php.stub' => $this->getMigrationFileName($filesystem),
-            ], 'migrations');
+            $this->publicConfigurations();
         }
+
+        $this->publicMigrations();
 
         if ($this->app->runningInConsole()) {
             $this->commands([
                 Commands\Notifications\WorkerStart::class,
             ]);
+        }
+    }
+
+    /**
+     * @author Rafael Agustin Rangel Sandoval <rarangels93@gmail.com>
+     */
+    private function publicConfigurations()
+    {
+        $this->publishes([
+            __DIR__.'/../config/api-basica.php' => config_path('api-basica.php'),
+        ], 'config');
+    }
+
+    private function publicMigrations(Filesystem $filesystem)
+    {
+        $migrations = [
+            'create_applications_table.php',
+            'create_configurations_table.php',
+        ];
+        foreach ($migrations as $migration) {
+            $this->publishes([
+                __DIR__.'/../database/migrations/'.$migration.'.stub' => $this->getMigrationFileName($filesystem, $migration),
+            ], 'migrations');
         }
     }
 
@@ -58,14 +72,14 @@ class ApiBasicaServiceProvider extends ServiceProvider
         $this->mergeConfigFrom(__DIR__.'/../config/api-basica.php', 'api-basica');
     }
 
-    protected function getMigrationFileName(Filesystem $filesystem): string
+    protected function getMigrationFileName(Filesystem $filesystem, $migration): string
     {
         $timestamp = date('Y_m_d_His');
 
         return Collection::make($this->app->databasePath().DIRECTORY_SEPARATOR.'migrations'.DIRECTORY_SEPARATOR)->flatMap(function (
-                $path
-            ) use ($filesystem) {
-                return $filesystem->glob($path.'*_create_applications_table.php');
-            })->push($this->app->databasePath()."/migrations/{$timestamp}_create_applications_table.php")->first();
+            $path
+        ) use ($filesystem, $migration) {
+            return $filesystem->glob($path.'*_'.$migration);
+        })->push($this->app->databasePath()."/migrations/{$timestamp}_{$migration}")->first();
     }
 }
